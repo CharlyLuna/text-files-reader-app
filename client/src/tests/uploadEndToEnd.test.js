316929e182a302c36978accf40a0353585c3fedc
CharlyLuna <cluna10@ucol.mx>
2023-05-22 21:25:40 -0600

    Fix test end to end

--- client/src/tests/uploadEndToEnd.test.js
+++ client/src/tests/uploadEndToEnd.test.js
@@ -1,15 +1,18 @@
+/* eslint-disable no-undef */
 import { Selector } from "testcafe";
 
-fixture("Upload File").page("http://localhost:5173/upload");
+fixture("Upload File").page("https://text-file-reader-app.netlify.app/");
 
 test("Send files to the database", async (t) => {
+  const goToUploadPageButton = Selector("button").withText(
+    "Go to upload file page"
+  );
+  await t.click(goToUploadPageButton);
   // Select the file input
   const fileInput = Selector("input[type=file]");
-
+  const filePath = "./512194438.txt";
   // Upload files
-  await t
-    .setFilesToUpload(fileInput, ["./512194438.txt"])
-    .click("#send-button");
+  await t.setFilesToUpload(fileInput, filePath).click("#send-button");
 
   const successMessage = Selector(".text-green-500");
   await t.expect(successMessage.exists).ok();
